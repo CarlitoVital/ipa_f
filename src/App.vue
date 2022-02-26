a16544c826ae5ab52094a1804c0292b0b7daf77c
CarlitoVital <62419871+CarlitoVital@users.noreply.github.com>
2022-02-26 11:50:54 +0100

    fonts link new

--- src/App.vue
+++ src/App.vue
@@ -65,43 +65,6 @@
 </template>
 
 <script>
-// const OFFSET = 60;
-
-// export default {
-//   data() {
-//     return {
-//       burger: false,
-//       showNavbar: true,
-//       lastScrollPosition: 0,
-//       scrollValue: 0,
-//     };
-//   },
-//   mounted() {
-//     this.lastScrollPosition = window.pageYOffset;
-//     window.addEventListener("scroll", this.onScroll);
-//     const viewportMeta = document.createElement("meta");
-//     viewportMeta.name = "viewport";
-//     viewportMeta.content = "width=device-width, initial-scale=1";
-//     document.head.appendChild(viewportMeta);
-//   },
-
-//   beforeUnmount() {
-//     window.removeEventListener("scroll", this.onScroll);
-//   },
-
-//   methods: {
-//     onScroll() {
-//       if (window.pageYOffset < 0) {
-//         return;
-//       }
-//       if (Math.abs(window.pageYOffset - this.lastScrollPosition) < OFFSET) {
-//         return;
-//       }
-//       this.showNavbar = window.pageYOffset < this.lastScrollPosition;
-//       this.lastScrollPosition = window.pageYOffset;
-//     },
-//   },
-// };
 </script>
 
 <style>
