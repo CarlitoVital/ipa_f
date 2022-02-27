9cce1eadaf832f4e6acc492db81789aae533d13e
CarlitoVital <62419871+CarlitoVital@users.noreply.github.com>
2022-02-27 20:49:45 +0100

    no text

--- src/views/LinkList.vue
+++ src/views/LinkList.vue
@@ -90,7 +90,7 @@
     <LinkCard v-for="link in filteredLinks" :key="link.id" :link="link" />
   </div>
   <div class="linkList loadingSpinner" v-else>
-    <p class="loadingSpinnerText">Inhalte werden geladen.</p>
+    <p class="startText loadingSpinnerText">Inhalte werden geladen.</p>
     <div class="lds-spinner">
       <div></div>
       <div></div>
